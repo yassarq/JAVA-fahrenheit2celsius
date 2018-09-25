389500204ebb14b907685a9b5aebf7210a7353a7
yassarq <yassarq8@gmail.com>
2018-09-25 18:56:19 -0400

    First commit

---
+++ FahrenheitToCelsius.java
@@ -0,0 +1,24 @@
+// # Name:       Yassar Qureshi
+// # Class:      CIS-2751
+// # Assignment: Homework 2 (2.1) Fahrenheit to Celsius
+// # File:       FahrenheitToCelsius.java
+// # Purpose:    Temperature convertion to Celsius
+
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+    public static void main(String[] args) {
+        // Create a Scanner object 
+        Scanner value = new Scanner(System.in);
+
+        // Prompt the user to enter the degree in Fahrenheit
+        System.out.print("Enter the degree in Fahrenheit: ");
+        double fahrenheit = input.nextDouble();
+
+        // Conversion from Celsius to Celsius 
+        double celsius = 5/9.0 * (fahrenheit - 32);
+
+        // Display results
+        System.out.println(fahrenheit + " in fahrenheit is " + celsius + " in celsius.");
+    }
+}
